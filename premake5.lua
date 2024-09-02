3f42df09fc389a5480207e5b979dbb7ed0927bbc
KJK <kody.j.king@gmail.com>
2024-09-02 03:14:32 -0700

    eom

--- premake5.lua
+++ premake5.lua
@@ -11,21 +11,23 @@ workspace "premake-template"
         system "Windows"
         architecture "x86_64"
 
--- Include all premake projects in the vendor directory
+local function isIncludable(path)
+    if string.find(path, "~") then return false end
+    return os.isfile(path .. "/premake5.lua")
+end
+
 group "vendor"
-    local vendorProjects = os.matchdirs("vendor/*")
-    for i, project in ipairs(vendorProjects) do
-        -- Check that the project has a premake5.lua file
-        if os.isfile(project .. "/premake5.lua") then
-            print("\27[95mIncluding vendor project: " .. project .. "\27[0m\n")
+    for i, project in ipairs(os.matchdirs("vendor/*")) do
+        if isIncludable(project) then
+            print("\27[95mIncluding: " .. project .. "\27[0m")
             include (project)
         end
     end
 group ""
 
--- Include all projects in the projects directory
-local projects = os.matchdirs("projects/*")
-for i, project in ipairs(projects) do
-    print("\27[34mIncluding project: " .. project .. "\27[0m\n")
-    include (project)
+for i, project in ipairs(os.matchdirs("projects/*")) do
+    if isIncludable(project) then
+        print("\27[34mIncluding: " .. project .. "\27[0m")
+        include (project)
+    end
 end
